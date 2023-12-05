84b16b1203622a1a4b1d45bf023e3ea4ab03a72c
konbraphat51 <konbraphat@gmail.com>
2023-12-05 17:25:19 +0900

    Add: show grids

--- Src/main.js
+++ Src/main.js
@@ -3,6 +3,9 @@ StopAllTouchDefaults();
 var edges = []
 var flagClicking = false;
 
+var grids_x = []
+var grids_y = []
+
 function CanvasToSystem(position) {
     return [
         position[0] - GetCanvasSize()[0] / 2,
@@ -32,10 +35,14 @@ function Draw() {
     SetColor("#AFEEEE")
     DrawPolygon(edges)
 
-    //grids
-    //center
+    //center point
     SetColor("red")
     DrawCircleByVec(SystemToCanvas([0, 0]), 5)
+
+    //grid
+    if (GetCheckBoxValue("showingGrid", true)) {
+        ShowGrids()
+    }
 
     // show mouse position
     SetColor("black")
@@ -44,14 +51,68 @@ function Draw() {
     DrawText("Mouse: " + mouse_system_position[0] + ", " + mouse_system_position[1], 50, 50)
 }
 
+function ShowGrids() {
+    let grid_interval = 10
+
+    let x_n = GetCanvasSize()[0] / grid_interval + 1
+    let y_n = GetCanvasSize()[1] / grid_interval + 1
+
+    SetColor("gray")
+
+    grids_x = []
+    for (let cnt = -Math.floor(x_n / 2); cnt < Math.floor(x_n / 2); cnt++) {
+        let x = grid_interval * cnt
+        grids_x.push(x)
+
+        let canvas_x = SystemToCanvas([x, 0])[0]
+
+        DrawLine(canvas_x, 0, canvas_x, GetCanvasSize()[1])
+    }
+
+    grids_y = []
+    for (let cnt = -Math.floor(y_n / 2); cnt < Math.floor(y_n / 2); cnt++) {
+        let y = grid_interval * cnt
+        grids_y.push(y)
+
+        let canvas_y = SystemToCanvas([0, y])[1]
+
+        DrawLine(0, canvas_y, GetCanvasSize()[0], canvas_y)
+    }
+}
+
 function Control() {
     // if clicked...
     if (GetMouse() & !flagClicking) {
         flagClicking = true;
-        AddEdge(GetMouseX(), GetMouseY())
+
+        if (GetCheckBoxValue("showingGrid", true)) {
+            let positionPutting = SystemToCanvas(FindNearestGrid(CanvasToSystem([GetMouseX(), GetMouseY()])))
+            AddEdge(positionPutting[0], positionPutting[1])
+        } else {
+            AddEdge(GetMouseX(), GetMouseY())
+        }
     } else if (!GetMouse()) {
         flagClicking = false;
     }
+}
+
+function FindNearestGrid(positionSystem) {
+    let nearest_x = grids_x[0]
+    let nearest_y = grids_y[0]
+
+    grids_x.forEach(x => {
+        if (Math.abs(x - positionSystem[0]) < Math.abs(nearest_x - positionSystem[0])) {
+            nearest_x = x
+        }
+    })
+
+    grids_y.forEach(y => {
+        if (Math.abs(y - positionSystem[1]) < Math.abs(nearest_y - positionSystem[1])) {
+            nearest_y = y
+        }
+    })
+
+    return [nearest_x, nearest_y]
 }
 
 function AddEdge(x, y) {
@@ -59,6 +120,8 @@ function AddEdge(x, y) {
 }
 
 async function main() {
+    PutCheckBox("showingGrid", "Show Grid", false)
+
     while (true) {
         Draw()
         Control()
